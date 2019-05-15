7946537ddebb77d376479a05ca0e50dd2406a8b7
hoangtubatu123 <kiemkhachdatinh2@gmail.com>
2019-05-14 21:57:20 -0700

    fix xong hashfile

--- src/components/Loading/API.js
+++ src/components/Loading/API.js
@@ -1,11 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 import constants from "../../constants";
 
-export const normalAPI =  axios.create({
-    baseURL:  constants.BASE_URL,
-    timeout: 1000,
-    headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-    }
-})
+export const normalAPI = axios.create({
+  baseURL: constants.BASE_URL,
+  timeout: 1000,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+export const formDataAPI = axios.create({
+  baseURL: constants.BASE_URL,
+  timeout: 1000,
+  headers: {
+    "Content-Type": "multipart/form-data"
+  }
+});
--- src/screens/Verification/index.js
+++ src/screens/Verification/index.js
@@ -1,11 +1,69 @@
-import React from 'react';
-
+import React from "react";
+import "./style.css";
+import $ from "jquery";
+import Loading from "../../components/Loading";
+import { Button } from "react-bootstrap";
 export default class Verification extends React.Component {
+  constructor() {
+    super();
+    this.submit = React.createRef();
+    this.state = {
+      fileInfo: ""
+    };
+  }
+  chooseFile = e => {
+    e.preventDefault();
+    $("input").click();
+  };
+  onChangeFile = e => {
+    this.setState({ fileInfo: e.target.files[0].name });
+    this.file = e.target.files[0];
+  };
+  onSubmit = () => {
+    if (!this.file) {
+      return alert("Bạn chưa nhập file");
+    }
+    let bodyFromData = new FormData();
+    bodyFromData.append("file", this.file);
+    if (this.submit && this.submit.current) {
+      this.submit.current.requestAPI(bodyFromData);
+    }
+  };
+  upFileSuccess = response => {
+    alert("upfile Success");
+    console.log(response.data);
+  };
+  upFileError = error => {
+    alert(JSON.stringify(error));
+  };
   render() {
     return (
-      <div>
-        <h1>This is Verification page.</h1>
-        <h2>Hi~~~</h2>
+      <div class="container">
+        <h5>Yêu cầu nhập file</h5>
+
+        <div class="input-container" style={{ marginTop: 10 }}>
+          <input type="file" id="input" onChange={this.onChangeFile} />
+          <button class="browse-btn" onClick={this.chooseFile}>
+            Chọn File
+          </button>
+          <span class="file-info">{this.state.fileInfo}</span>
+        </div>
+        <Button
+          variant="dark"
+          size="lg"
+          style={{ marginTop: 50 }}
+          onClick={this.onSubmit}
+        >
+          Submit
+        </Button>
+        <Loading
+          ref={this.submit}
+          url="/verification/getFile"
+          onSuccess={this.upFileSuccess}
+          onError={this.onError}
+          isFormData
+          method="POST"
+        />
       </div>
     );
   }
